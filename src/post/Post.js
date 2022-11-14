027448dabd1a60c8e293870ef38bbb746c8646a8
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2022-11-14 21:38:16 +0530

    post

--- src/post/Post.js
+++ src/post/Post.js
@@ -13,6 +13,7 @@ function post() {
         <hr />
         <span className="postDate">1 Hour ago</span>
       </div>
+      <p className='postDesc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, eaque blanditiis nam impedit quos minus, cum alias eum exercitationem maiores officiis natus quibusdam molestias quis enim perferendis mollitia distinctio. Velit, vero corrupti necessitatibus optio perspiciatis nam officiis similique, reprehenderit quos laboriosam numquam eum reiciendis modi voluptates, recusandae tempora minus facere!</p>
     </div>
   )
 }
