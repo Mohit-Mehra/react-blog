1a147862c66b5bb6c2d36f43abe70ea75fb02252
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2022-11-15 21:34:16 +0530

    15-11-22

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -3,13 +3,13 @@ import './post.css'
 function post() {
   return (
     <div className='post'>
-      <img className='postImg' src="https://source.unsplash.com/random/?Music/" alt="" />
+      <img className='postImg' src="https://source.unsplash.com/random/?music/" alt="" />
       <div className="postInfo">
         <div className="postCats">
             <span className="postCats">Music</span>
             <span className="postCats">Life</span>
         </div>
-        <span className="postTitle">Lorem ipsum dolor sit</span>
+        <span className="postTitle">Lorem ipsum dolor sit</span> 
         <hr />
         <span className="postDate">1 Hour ago</span>
       </div>
---
+++ src/components/singlePost/SinglePost.js
@@ -0,0 +1,14 @@
+import "./singlePost.css"
+
+function SinglePost() {
+  return (
+    <div className="singlePost">
+      <div className="singlePostWrapper">
+        <img src="https://source.unsplash.com/random/?coding/" alt="" className="singlePostImg" />
+        <h1 className="singlePostTitle">Lorem ipsum dolor sit amet.</h1>
+      </div>
+    </div>
+  )
+}
+
+export default SinglePost
