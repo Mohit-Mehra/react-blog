6a09bf72f6518708d90a005355e7340b1b8db713
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2022-11-20 16:57:30 +0530

    connection

--- api/index.js
+++ api/index.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv")
+const mongoose = require("mongoose")
+dotenv.config();
+const connection_string = process.env.MONGO_URL
 
-app.use("/",(req,res)=>{
-    console.log("hey world")
+mongoose.connect(connection_string, {
+    useNewUrlParser: true,
+    // useCreateIndex: true,
+    useUnifiedTopology: true,
 })
+.then(() => console.log('MongoDB connection established.'))
+.catch((error) => console.error("MongoDB connection failed:", error.message))
 
 app.listen("5000",()=>{
     console.log("hello")
