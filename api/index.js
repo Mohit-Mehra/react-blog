c24393a0f64ab6d0111e5b60b49487f0fa9a4dae
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2022-11-25 22:04:16 +0530

    postman

--- api/index.js
+++ api/index.js
@@ -2,7 +2,12 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv")
 const mongoose = require("mongoose")
+const authRoute = require("./routes/auth")
+
 dotenv.config();
+app.use(express.json())
+
+
 const connection_string = process.env.MONGO_URL
 
 mongoose.connect(connection_string, {
@@ -12,6 +17,8 @@ mongoose.connect(connection_string, {
 })
 .then(() => console.log('MongoDB connection established.'))
 .catch((error) => console.error("MongoDB connection failed:", error.message))
+
+app.use("/api/auth",authRoute)
 
 app.listen("5000",()=>{
     console.log("hello")
