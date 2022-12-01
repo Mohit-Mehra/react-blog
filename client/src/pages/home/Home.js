7140c11943972c23ad8cfbe40f09fd8a5dea2664
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2022-12-01 19:00:49 +0530

    connection start

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -2,9 +2,20 @@ import './home.css'
 import Header from "../../components/header/Header";
 import SideBar from '../../components/sidebar/SideBar';
 import Posts from '../../components/posts/Posts';
+import { useEffect, useState } from "react"
+import axios from "axios"
 
 
 function Home() {
+  const [posts, setPosts] = useState([])
+
+  useEffect(()=>{
+    const fetchPosts = async()=>{
+      const res = await axios.get("/posts")
+      console.log(res)
+    }
+    fetchPosts()
+  },[])
   return (
     <div>
       <Header />
