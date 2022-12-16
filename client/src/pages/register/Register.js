14bcbb2bce9f167644448f4266afe1765716b583
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2022-12-16 20:49:41 +0530

    register page

--- client/src/pages/register/Register.js
+++ client/src/pages/register/Register.js
@@ -7,17 +7,18 @@ const Register = () => {
   const [username,setUsername] = useState("")
   const [email,setEmail] = useState("")
   const [password,setPassword] = useState("")
+  // const hundleSubmit = 
   return (
     <div className="register">
         <span className="registerTitle">Register</span>
-      <form className="registerForm">
+      <form className="registerForm" onSubmit={handleSubmit}>
         <label>Username</label>
-        <input type="text" className="registerInput" placeholder="Enter your username" />
+        <input type="text" className="registerInput" placeholder="Enter your username" onChange={e=>setUsername(e.target.value)} />
         <label>Email</label>
-        <input type="email" className="registerInput" placeholder="Enter your Email" />
+        <input type="email" className="registerInput" placeholder="Enter your Email" onChange={e=>setEmail(e.target.value)} />
         <label>Password</label>
-        <input type="password" className="registerInput" placeholder="Enter your Password" />
-        <button className="registerButton">register</button>
+        <input type="password" className="registerInput" placeholder="Enter your Password" onChange={e=>setPassword(e.target.value)} />
+        <button className="registerButton" type="submit">register</button>
       </form>
       <button className="registerLoginButton">
       <Link className="link" to="/login">Login</Link>
