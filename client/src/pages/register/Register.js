7b85906221ef3de7ceb84502ad2ebca02a53de31
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2022-12-17 21:36:32 +0530

    register system complete

--- client/src/pages/register/Register.js
+++ client/src/pages/register/Register.js
@@ -8,14 +8,20 @@ const Register = () => {
   const [username,setUsername] = useState("")
   const [email,setEmail] = useState("")
   const [password,setPassword] = useState("")
+  const [error,setError] = useState(false)
   const handleSubmit = async (e)=>{
     e.preventDefault()
-    const res = await axios.post("/auth/register",{
-      username,
-      email,
-      password
-    })
-    console.log(res)
+    setError(false)
+    try{
+      const res = await axios.post("/auth/register",{
+        username,
+        email,
+        password
+      })
+      res.data && window.location.replace("/login")
+    } catch(err){ 
+      setError(true)
+    }
   }
   return (
     <div className="register">
@@ -32,7 +38,7 @@ const Register = () => {
       <button className="registerLoginButton">
       <Link className="link" to="/login">Login</Link>
       </button>
-      
+      {error && <span style={{color:'red',marginTop:"10px"}}>Something went wrong!</span>}
     </div>
   )
 }
