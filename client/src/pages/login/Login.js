8bb89cdd8df92f27c53feaabb45730d982169ccb
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2022-12-23 20:58:06 +0530

    complete context API

--- client/src/pages/login/Login.js
+++ client/src/pages/login/Login.js
@@ -2,15 +2,16 @@ import { Link } from "react-router-dom"
 import "./login.css"
 
 const Login = () => {
+  const handleSubmit = false
   return (
     <div className="login">
         <span className="loginTitle">Login</span>
-      <form className="loginForm">
-        <label>Email</label>
-        <input type="email" className="loginInput" placeholder="Enter your Email" />
+      <form className="loginForm" onSubmit={handleSubmit}>
+        <label>Username</label>
+        <input type="email" className="loginInput" placeholder="Enter your Username" />
         <label>Password</label>
         <input type="password" className="loginInput" placeholder="Enter your Password" />
-        <button className="loginButton">Login</button>
+        <button className="loginButton" type="submit">Login</button>
       </form>
       <button className="loginRegisterButton">
       <Link className="link" to="/register">Register</Link>
