798cd8ad73e0930d84a2956a64e28c0216bf24ef
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2023-01-01 10:10:21 +0530

    use localstorage with context API

--- client/src/pages/login/Login.js
+++ client/src/pages/login/Login.js
@@ -7,7 +7,7 @@ import axios from 'axios';
 const Login = () => {
   const userRef = useRef()
   const passwordRef = useRef()
-  const {user,dispatch ,isFetching} = useContext(Context)
+  const {dispatch ,isFetching} = useContext(Context)
 
   const handleSubmit = async (e) => {
     e.preventDefault()
@@ -23,7 +23,6 @@ const Login = () => {
       dispatch({type:'LOGIN_FAILURE'})
     }
   }
-  console.log(user)
   return (
     <div className="login">
         <span className="loginTitle">Login</span>
@@ -32,7 +31,7 @@ const Login = () => {
         <input type="text" className="loginInput" placeholder="Enter your Username" ref={userRef} />
         <label>Password</label>
         <input type="password" className="loginInput" placeholder="Enter your Password" ref={passwordRef} />
-        <button className="loginButton" type="submit">Login</button>
+        <button className="loginButton" type="submit" disabled={isFetching}>Login</button>
       </form>
       <button className="loginRegisterButton">
       <Link className="link" to="/register">Register</Link>
