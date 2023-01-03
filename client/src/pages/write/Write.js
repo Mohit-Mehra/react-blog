2ccdbfe53435471e01f82a11ca02b1ec4273d9cf
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2023-01-03 21:40:34 +0530

    upload image

--- client/src/pages/write/Write.js
+++ client/src/pages/write/Write.js
@@ -1,42 +1,55 @@
 import "./write.css"
-import {useState,useContext} from 'react'
+import { useState, useContext } from 'react'
 import axios from 'axios'
-import {Context} from '../../context/Context'
+import { Context } from '../../context/Context'
 
 function Write() {
-  const [title,setTitle] = useState("")
-  const [desc,setDesc] = useState("")
-  const [file,setFile] = useState("")
-  const {user} = useContext(Context)
+  const [title, setTitle] = useState("")
+  const [desc, setDesc] = useState("")
+  const [file, setFile] = useState("")
+  const { user } = useContext(Context)
 
-  const handleSubmit = (e)=>{
+  const handleSubmit = async (e) => {
     e.preventDefault()
     const newPost = {
-      username:user.username,
+      username: user.username,
       title,
-      desc
+      desc,
     }
-    if(file){
-      const data = FormData()
+    if (file) {
+      const data = new FormData()
       const filename = Date.now() + file.name
-      data.append("name",filename)
-      data.append("file",file)
+      data.append("name", filename)
+      data.append("file", file)
+      newPost.photo = filename
+      try {
+        await axios.post("/upload",data)
+      } catch (err) {
+        
+      }
     }
-    axios.post("/posts",)
+    try {
+       const res = await axios.post("/posts",newPost)
+       window.location.replace("/post/"+res.data._id)
+    } catch (err) {
+      
+    }
   }
   return (
     <div className="write">
-        <img className="writeImg" src="https://source.unsplash.com/random/?galaxy/" alt="" />
+      {file &&
+      <img className="writeImg" src={URL.createObjectURL(file)} alt="" />
+      }
       <form className="writeForm" onSubmit={handleSubmit}>
         <div className="writeFormGroup">
-            <label htmlFor="fileInput">
+          <label htmlFor="fileInput">
             <i className="Icon fa-solid fa-plus"></i>
-            </label>
-            <input type="file" id="fileInput" style={{display:"none"}} />
-            <input type="text" placeholder="Title" className="writeInput" autoFocus={true}/>
+          </label>
+          <input type="file" id="fileInput" style={{ display: "none" }} onChange={(e)=> setFile(e.target.files[0])} />
+          <input type="text" placeholder="Title" className="writeInput" autoFocus={true} onChange={e=>setTitle(e.target.value)} />
         </div>
         <div className="writeFormGroup">
-            <textarea placeholder="Tell your story........." type="text" className="writeInput writeText"></textarea>
+          <textarea placeholder="Tell your story........." type="text" className="writeInput writeText" onChange={e=>setDesc(e.target.value)}></textarea>
         </div>
         <button className="writeSubmit" type="submit">Publish</button>
       </form>
